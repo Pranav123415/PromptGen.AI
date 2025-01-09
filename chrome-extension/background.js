81648cfd57eb2130c8ae460d3cee5516148411a5
Pranav123415 <pranav.kumar1896@gmail.com>
2025-01-09 21:11:09 +0530

    Remove feature

--- chrome-extension/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'STORE_PROMPT') {
-    chrome.storage.local.set({ prompt: request.prompt });
-  }
-}); 
